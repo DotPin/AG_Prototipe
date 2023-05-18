bef331f6c6ba0864dd5e33cebaaaf09092dd6f76
dotpin <diego.rojas.asenjo@alumnos.uach.cl>
2023-05-18 13:55:22 -0400

    Actualizando algunos datos

---
+++ module/Aleatorio.h
@@ -0,0 +1,18 @@
+#ifndef ALEATORIO_H
+#define ALEATORIO_H
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Aleatorio
+{
+    protected:
+        
+    public:
+        Aleatorio(){};
+        ~Aleatorio(){};
+        float random(float a, float b);
+};
+
+#endif
